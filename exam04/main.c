d6f3b09de08700dbb18835ceadcf202a8a576f7c
woolimi <woolim.park@hotmail.com>
2020-07-08 19:21:31 +0200

    mod

--- exam04/main.c
+++ exam04/main.c
@@ -6,23 +6,24 @@
 #include <sys/wait.h>
 #include <stdio.h>
 
-typedef struct	s_sh
+
+typedef struct s_sh
 {
-	char		**cmd;
-	char		**env;
-	int			idx;
-	int			excode;
-}				t_sh;
+	char **cmd;
+	char **env;
+	int idx;
+	int excode;
+} t_sh;
 
 typedef enum e_error
 {
 	FATAL,
 	EXE,
-	CD_BAD_ARG,
-	CD_CHANGE_DIR,
+	BAD_ARG,
+	CHANGE_DIR
 } t_error;
 
-int	ft_strequ(char *s1, char *s2)
+int ft_strequ(char *s1, char *s2)
 {
 	if (strcmp(s1, s2) == 0)
 		return (1);
@@ -31,9 +32,7 @@ int	ft_strequ(char *s1, char *s2)
 
 int ft_strlen(char *s)
 {
-	int i;
-
-	i = 0;
+	int i = 0;
 	while (s[i])
 		i++;
 	return (i);
@@ -44,19 +43,19 @@ void ft_putstr_fd(char *s, int fd)
 	write(fd, s, ft_strlen(s));
 }
 
-int str_error(int error_code, char *arg)
+int str_error(int ecode, char *arg)
 {
-	if (error_code == FATAL)
+	if (ecode == FATAL)
 		ft_putstr_fd("error: fatal\n", 2);
-	if (error_code == EXE)
+	if (ecode == EXE)
 	{
 		ft_putstr_fd("error: cannot execute ", 2);
 		ft_putstr_fd(arg, 2);
 		ft_putstr_fd("\n", 2);
 	}
-	if (error_code == CD_BAD_ARG)
+	if (ecode == BAD_ARG)
 		ft_putstr_fd("error: cd: bad arguments\n", 2);
-	if (error_code == CD_CHANGE_DIR)
+	if (ecode == CHANGE_DIR)
 	{
 		ft_putstr_fd("error: cd: cannot change directory to ", 2);
 		ft_putstr_fd(arg, 2);
@@ -67,13 +66,8 @@ int str_error(int error_code, char *arg)
 
 char **make_arg(t_sh *sh)
 {
-	int cnt;
-	int i;
-	int idx;
-	char **res;
-
-	cnt = 0;
-	idx = sh->idx;
+	int cnt = 0;
+	int idx = sh->idx;
 	while (sh->cmd[sh->idx]
 		&& !ft_strequ(sh->cmd[sh->idx], ";")
 		&& !ft_strequ(sh->cmd[sh->idx], "|"))
@@ -81,55 +75,39 @@ char **make_arg(t_sh *sh)
 		cnt++;
 		sh->idx++;
 	}
-	if (!(res = malloc(sizeof(char*) * (cnt + 1))))
+	char **res = malloc(sizeof(char*) * (cnt + 1));
+	if (!res)
 		return (0);
-	i = 0;
+	int i = 0;
 	while (i < cnt)
 		res[i++] = sh->cmd[idx++];
 	res[i] = 0;
-	return (res); 
+	return (res);
 }
 
-void exec_non_btin(t_sh *sh, char **arg)
+void cd_btin(t_sh *sh)
 {
-	execve(arg[0], arg, sh->env);
-	str_error(EXE, arg[0]);
-	exit(EXIT_FAILURE);
-}
-
-void get_exit_code(t_sh *sh, int status)
-{
-	if (WIFEXITED(status))
-		sh->excode = WEXITSTATUS(status);
-	else if (WIFSIGNALED(status))
-		sh->excode = 128 + WTERMSIG(status);
-}
-
-void non_btin(t_sh *sh)
-{
-	int cpid;
-	int status;
-	char **arg;
-
-	if (!(arg = make_arg(sh)))
+	char **arg = make_arg(sh);
+	if (!arg)
 		exit(str_error(FATAL, NULL));
-	if ((cpid = fork()) == -1)
-		exit(str_error(FATAL, NULL));
-	if (cpid == 0)
-		exec_non_btin(sh, arg);
+
+	int cnt = 0;
+	while (arg[cnt])
+		cnt++;
+	if (cnt != 2)
+		sh->excode = str_error(BAD_ARG, NULL);
+	else if (chdir(arg[1]) == -1)
+		sh->excode = str_error(CHANGE_DIR, NULL);
 	else
-	{
-		free(arg);
-		wait(&status);
-		get_exit_code(sh, status);
-	}
+		sh->excode = 0;
+	free(arg);
 }
 
 int count_pipes(t_sh *sh)
 {
 	int i;
 	int cnt;
-	
+
 	cnt = 0;
 	i = sh->idx;
 	while (sh->cmd[i] && !ft_strequ(sh->cmd[i], ";"))
@@ -143,12 +121,10 @@ int count_pipes(t_sh *sh)
 
 void create_pipes(int pipes[], int cnt)
 {
-	int i;
-
-	i = 0;
+	int i = 0;
 	while (i < cnt)
 	{
-		if (pipe(pipes + (i * 2)) == -1)
+		if (pipe(pipes + (i * 2)) < 0)
 			exit(str_error(FATAL, NULL));
 		i++;
 	}
@@ -157,7 +133,6 @@ void create_pipes(int pipes[], int cnt)
 void close_all(int pipes[], int cnt)
 {
 	int i = 0;
-
 	while (i < 2 * cnt)
 		close(pipes[i++]);
 }
@@ -169,6 +144,20 @@ void dup2_and_close_pipes(int pipes[], int i, int cnt)
 	if (i > 0)
 		dup2(pipes[(i - 1) * 2], 0);
 	close_all(pipes, cnt);
+}
+
+void exec_non_btin(t_sh *sh, char **arg)
+{
+	execve(arg[0], arg, sh->env);
+	exit(str_error(EXE, arg[0]));
+}
+
+void get_exit_code(t_sh *sh, int status)
+{
+	if (WIFEXITED(status))
+		sh->excode = WEXITSTATUS(status);
+	else if (WIFSIGNALED(status))
+		sh->excode = 128 + WTERMSIG(status);
 }
 
 void close_pipes_and_wait(t_sh *sh, int pipes[], int cnt, int cpids[])
@@ -188,25 +177,25 @@ void close_pipes_and_wait(t_sh *sh, int pipes[], int cnt, int cpids[])
 
 void piping(t_sh *sh, int cnt)
 {
-	int pipes[cnt * 2];
-	int cpids[cnt + 1];
-	int i;
-	char **arg;
+	int pipes[cnt * 2]; // nb pipe * 2
+	int cpids[cnt + 1]; // A | B
 
 	create_pipes(pipes, cnt);
-	i = 0;
-	while (i < (cnt + 1))
+	int i = 0;
+	char **arg;
+	while (i < cnt + 1)
 	{
 		if (ft_strequ(sh->cmd[sh->idx], "|"))
-			sh->idx++;		
-		if (!(arg = make_arg(sh)))
+			sh->idx++;
+		arg = make_arg(sh);
+		if (!arg)
 			exit(str_error(FATAL, NULL));
-		if ((cpids[i] = fork()) == 0)
+		cpids[i] = fork();
+		if (cpids[i] == 0)
 		{
 			dup2_and_close_pipes(pipes, i, cnt);
 			exec_non_btin(sh, arg);
-		}
-		else if (cpids[i] == -1)
+		} else if (cpids[i] < 0)
 			exit(str_error(FATAL, NULL));
 		free(arg);
 		i++;
@@ -214,23 +203,25 @@ void piping(t_sh *sh, int cnt)
 	close_pipes_and_wait(sh, pipes, cnt, cpids);
 }
 
-void cd_btin(t_sh *sh)
+void non_btin(t_sh *sh)
 {
+	int cpid;
+	int status;
 	char **arg = make_arg(sh);
-	int cnt;
 
 	if (!arg)
 		exit(str_error(FATAL, NULL));
-	cnt = 0;
-	while (arg[cnt])
-		cnt++;
-	if (cnt == 1 || cnt >= 3)
-		sh->excode = str_error(CD_BAD_ARG, NULL);
-	else if (chdir(arg[1]) == -1)
-		sh->excode = str_error(CD_CHANGE_DIR, arg[1]);
+	cpid = fork();
+	if (cpid < 0)
+		exit(str_error(FATAL, NULL));
+	if (cpid == 0)
+		exec_non_btin(sh, arg);
 	else
-		sh->excode = 0;
-	free(arg);
+	{
+		free(arg);
+		wait(&status);
+		get_exit_code(sh, status);
+	}
 }
 
 int main(int ac, char **cmd, char **env)
